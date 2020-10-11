8451c13af140d9e4f70ea43b1abba4380e38cba6
nemothecollector <coolcore.xix@gmail.com>
2020-10-11 18:09:57 +0700

    feat: can run with typescript, have hello world from the hapi solution

---
+++ packages/ntc-be/index.ts
@@ -0,0 +1,25 @@
+import Hapi from '@hapi/hapi';
+
+async function init() {
+    const server = Hapi.server({
+        port: 3001,
+        host: 'localhost',
+    });
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: (request, h) => {
+
+            return 'Hello World!';
+        }
+    });
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+}
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
---
+++ packages/ntc-be/start.js
@@ -0,0 +1,2 @@
+require("@babel/register")({extensions: ['.js', '.ts']});
+require("./index");
