350d9558ae69abc0b2cffab3d61e669ac6dd5388
phat <phat.pham@nfq.asia>
2021-03-16 13:36:53 +0700

    doc: update pinned

--- packages/ntc-fe/src/home-page-data/pinned-content.ts
+++ packages/ntc-fe/src/home-page-data/pinned-content.ts
@@ -2,6 +2,7 @@ export const pinnedContent = {
     caption: 'Healthy comtemplation at Cốc',
     images: [
         'https://i.imgur.com/g3k2oDF.jpeg',
+        'https://i.imgur.com/GadLLvJ.jpeg',
         'https://i.imgur.com/ejUGraN.jpg',
         'https://i.imgur.com/x74fT8R.jpg',
     ],
