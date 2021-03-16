f0317bcd75fe203351724ae0dd7d97b94fd1261c
phat <phat.pham@nfq.asia>
2021-03-16 13:35:14 +0700

    doc: update pinned

--- packages/ntc-fe/src/home-page-data/pinned-content.ts
+++ packages/ntc-fe/src/home-page-data/pinned-content.ts
@@ -1,7 +1,8 @@
 export const pinnedContent = {
-    caption: 'Back to Sài Gòn - double tempo!',
+    caption: 'Healthy comtemplation at Cốc',
     images: [
-        'https://i.imgur.com/N7CDwen.jpeg',
-        'https://i.imgur.com/VL4oMJX.jpeg',
+        'https://i.imgur.com/g3k2oDF.jpeg',
+        'https://i.imgur.com/ejUGraN.jpg',
+        'https://i.imgur.com/x74fT8R.jpg',
     ],
 }
