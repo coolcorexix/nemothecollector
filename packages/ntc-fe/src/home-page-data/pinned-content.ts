93367da6a42ec5dca3d111800181784970aa3acd
phatIntern <phat.pham@saleshood.com>
2022-09-15 21:17:42 +0700

    doc: description for better recruitment

--- packages/ntc-fe/src/home-page-data/pinned-content.ts
+++ packages/ntc-fe/src/home-page-data/pinned-content.ts
@@ -1,5 +1,5 @@
 export const pinnedContent = {
-  caption: 'Walking away from traffic jams and other bullshits',
+  caption: 'Walking away from traffic jams and other urban pains',
   images: [
     'https://i.imgur.com/HZ9vBNf.jpeg',
     'https://i.imgur.com/jcCwmaX.jpeg',
