309531c5eb2426640271231541ae1f0b59448e1a
coolcorexix <coolcore.xix@gmail.com>
2023-05-07 09:40:49 +0700

    Update pinned-content.ts

--- packages/ntc-fe/src/home-page-data/pinned-content.ts
+++ packages/ntc-fe/src/home-page-data/pinned-content.ts
@@ -1,7 +1,7 @@
 export const pinnedContent = {
   caption: 'Made it to the beach',
   images: [
-    'https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/329285444_916320023061648_5116502757056684156_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=0debeb&_nc_ohc=3_Mfa3gdSscAX9gjXYG&_nc_ht=scontent.fdad3-4.fna&oh=00_AfBC5F0qlViVEBIdEL7dvM6PgPOTG5Aou0uNoIc24rmcfw&oe=64527041',
-    'https://scontent.fdad3-5.fna.fbcdn.net/v/t39.30808-6/329574696_900267471113188_2026787594299619086_n.jpg?stp=cp6_dst-jpg&_nc_cat=106&ccb=1-7&_nc_sid=0debeb&_nc_ohc=A5G-265HTTAAX9iSVdW&_nc_ht=scontent.fdad3-5.fna&oh=00_AfDufQxpYvLW5b7KXALzWGMuyQcO3bOies5kWcsT-V05vA&oe=6453A8C9',
+    'https://i.imgur.com/j5auAzm.jpg',
+    'https://i.imgur.com/6FeNofw.jpg',
   ],
 };
