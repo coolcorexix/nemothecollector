711e11129bf1be1f6d8b2ebf7eb8fdc8c7715050
phatIntern <phat.pham@saleshood.com>
2024-05-17 11:07:50 +0700

    tag

--- packages/ntc-fe/pages/_app.js
+++ packages/ntc-fe/pages/_app.js
@@ -1,22 +1,21 @@
-import { useEffect } from 'react';
-import 'tailwindcss/tailwind.css';
+import { GoogleAnalytics } from '@next/third-parties/google';
 import 'antd/dist/antd.css';
-import ReactGA from 'react-ga';
+import 'tailwindcss/tailwind.css';
 import '../styles/globals.css';
-import './svg-playground/cube-style.scss';
 import './FolderTree.scss';
 import './a-piece-of-art/animation.css';
+import './svg-playground/cube-style.scss';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    ReactGA.initialize('G-8S8745E8GZ');
-    // To Report Page View
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   if (typeof window === 'undefined') {
     return null;
   }
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GoogleAnalytics gaId="G-8S8745E8GZ" />
+      <Component {...pageProps} />;
+    </>
+  );
 }
 
 export default MyApp;
